73628cfe8b550e6d6810a8dc45f61d4bc7bdf576
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-11-04 16:36:33 +0200

    Ruuvi DB type fixed

--- src/api/models/ruuviModel.ts
+++ src/api/models/ruuviModel.ts
@@ -3,20 +3,10 @@ import {Ruuvi} from '../../types/Ruuvi';
 
 const ruuviSchema = new Schema({
   data: {
-    data_format: {type: Number, required: true},
     humidity: {type: Number, required: true},
     temperature: {type: Number, required: true},
     pressure: {type: Number, required: true},
-    acceleration: {type: Number, required: true},
-    acceleration_x: {type: Number, required: true},
-    acceleration_y: {type: Number, required: true},
-    acceleration_z: {type: Number, required: true},
-    tx_power: {type: Number, required: true},
-    battery: {type: Number, required: true},
-    movement_counter: {type: Number, required: true},
-    measurement_sequence_number: {type: Number, required: true},
     mac: {type: String, required: true},
-    rssi: {type: Number, required: false},
   },
   timestamp: {type: Date, default: Date.now},
 });
--- src/types/Ruuvi.ts
+++ src/types/Ruuvi.ts
@@ -1,21 +1,11 @@
 type Ruuvi = {
   data: {
-    data_format: number;
     humidity: number;
     temperature: number;
     pressure: number;
-    acceleration: number;
-    acceleration_x: number;
-    acceleration_y: number;
-    acceleration_z: number;
-    tx_power: number;
-    battery: number;
-    movement_counter: number;
-    measurement_sequence_number: number;
     mac: string;
-    rssi: number | null;
-    date: Date;
   };
+  timestamp: Date;
 };
 
 export {Ruuvi};
