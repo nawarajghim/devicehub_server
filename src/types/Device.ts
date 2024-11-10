f983e7433e000f1bc0b940996532689fcaeb6c06
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-11-10 21:10:33 +0200

    device type fixes

--- src/types/Device.ts
+++ src/types/Device.ts
@@ -1,14 +1,14 @@
 type Device = {
-    name: string;
-    deviceClass: string;
-    deviceType: string;
-    location?: string;
-    settings?: string;
-    status: string;
-    data: {
-        [key: string]: string;
-    };
-    last_updated: Date;
-}
+  name: string;
+  deviceClass: string;
+  deviceType: string;
+  location?: string;
+  settings?: string;
+  status: string;
+  data: {
+    [key: string]: string | number | boolean;
+  };
+  last_updated: Date;
+};
 
 export {Device};
